2a7049b372f46ae06a9c3d04aa2235e9f5ab8abf
Superior_P <superior.prv@gmail.com>
2024-10-19 13:35:25 +0500

    signUp

--- src/pages/clientPages/signUp/SignUp.jsx
+++ src/pages/clientPages/signUp/SignUp.jsx
@@ -1,8 +1,21 @@
+import { Box, Button, TextField, Typography } from "@mui/material"
+import logo from "../../../../public/logo.svg"
 
 const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+  return <Box sx={{ background:"linear-gradient(to right , #A2D2FF , #CDB4DB )"}}>
+    <Box>
+        <img src={logo} alt="" />
+        <Typography>QuestioNet</Typography>
+    </Box>
+    <Box>
+        <Typography></Typography>
+        <TextField />
+        <TextField />
+        <TextField />
+        <TextField />
+        <Button></Button>
+    </Box>
+  </Box>
 }
 
 export default SignUp
