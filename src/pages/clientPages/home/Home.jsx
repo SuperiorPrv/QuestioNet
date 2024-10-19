76edd850b9e7c55a2e6473e2fdec605755edf0a4
Superior_P <superior.prv@gmail.com>
2024-10-19 19:31:33 +0500

    home started

--- src/pages/clientPages/home/Home.jsx
+++ src/pages/clientPages/home/Home.jsx
@@ -1,13 +1,9 @@
 import { Box } from "@mui/material"
-import Footer from "../../../components/shared/footer/Footer"
-import Header from "../../../components/shared/header/Header"
 
 const Home = () => {
   return (
     <Box>
-        <Header/>
         Body
-        <Footer/>
     </Box>
   )
 }
