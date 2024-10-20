001618cafa4cedde1f513ad53f4af8716df23554
Muhammad <muhammad20080516@gmail.com>
2024-10-20 09:54:07 +0500

    fix

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,7 +1,36 @@
+import { Box, Typography } from "@mui/material"
+import Logo from "../../../../public/Frame 6.png"
+import { Link, useLocation } from "react-router-dom"
 
 const Footer = () => {
+  const path=useLocation().pathname
+
   return (
-    <div>Footer</div>
+    <Box sx={{background:"#123456CC"}}>
+      <Box>
+        <img src={Logo} alt="" />
+        <Box sx={{display:"flex",gap:"40px"}}>
+  <Typography>
+    <Link style={path=="/dashboard/home"?{fontWeight:"900",textDecoration:"none" , color:"white"}:{fontWeight:"500",textDecoration:"none" ,  color:"white"}} to={"/dashboard/home"}>Home</Link>
+
+  </Typography>
+    <Typography>
+    <Link style={path=="/dashboard/question"?{fontWeight:"900",textDecoration:"none", color:"white"}:{fontWeight:"500",textDecoration:"none" ,  color:"white"}} to={"/dashboard/"}>Question</Link>
+
+    </Typography>
+    <Typography>
+    <Link style={path=="/dashboard/users"?{fontWeight:"900",textDecoration:"none", color:"white"}:{fontWeight:"500",textDecoration:"none",  color:"white"}} to={"/dashboard/home"}>Users</Link>
+
+    </Typography>
+    <Typography>
+    <Link style={path=="/dashboard/about  "?{fontWeight:"900",textDecoration:"none", color:"white"}:{fontWeight:"500",textDecoration:"none" ,  color:"white"}} to={"/dashboard/home"}>About</Link>
+
+
+    </Typography>
+
+  </Box>
+      </Box>
+    </Box>
   )
 }
 
