6640eec8b0375f8169652fdf532972a01d539d07
Mohhiraa <emscott.11@icloud.com>
2024-10-20 09:38:24 +0500

    up to date

--- src/components/shared/header/Header.jsx
+++ src/components/shared/header/Header.jsx
@@ -1,15 +1,24 @@
-import { Box, Typography } from "@mui/material"
+import { Avatar, Box, Button, Menu, MenuItem, Typography } from "@mui/material"
 import Logo from "../../../../public/Frame 6.png"
 import { Link, useLocation } from "react-router-dom"
-
+import { useState } from "react"
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
   const path=useLocation().pathname
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
    <>
 <Box sx={{display:"flex",alignItems:"center",justifyContent:"space-evenly",width:"80%",margin:"auto"}}>
   <Box><img src={Logo} alt="" /></Box>
-  <Box sx={{display:"flex",gap:"10px"}}>
+  <Box sx={{display:{xs:"none",md:"flex"},gap:"10px"}}>
   <Typography>
     <Link style={path=="/dashboard/home"?{fontWeight:"900",textDecoration:"none"}:{fontWeight:"500",textDecoration:"none"}} to={"/dashboard/home"}>Home</Link>
 
@@ -29,7 +38,56 @@ const Header = () => {
     </Typography>
 
   </Box>
-  <Box>
+  <Box sx={{display:{xs:"block",md:"none"}}}>
+<Button
+
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        <MenuIcon/>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+        <Typography>
+    <Link style={path=="/dashboard/home"?{fontWeight:"900",textDecoration:"none"}:{fontWeight:"500",textDecoration:"none"}} to={"/dashboard/home"}>Home</Link>
+
+  </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Typography>
+    <Link style={path=="/dashboard/question"?{fontWeight:"900",textDecoration:"none"}:{fontWeight:"500",textDecoration:"none"}} to={"/dashboard/home"}>Question</Link>
+
+    </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Typography>
+    <Link style={path=="/dashboard/users"?{fontWeight:"900",textDecoration:"none"}:{fontWeight:"500",textDecoration:"none"}} to={"/dashboard/home"}>Users</Link>
+
+    </Typography>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+        <Typography>
+    <Link style={path=="/dashboard/about  "?{fontWeight:"900",textDecoration:"none"}:{fontWeight:"500",textDecoration:"none"}} to={"/dashboard/home"}>About</Link>
+
+
+    </Typography>
+        </MenuItem>
+
+      </Menu>
+</Box>
+  <Box sx={{display:"flex",alignItems:"center",gap:"10px"}}>
+    <Avatar/>
 <Typography sx={{fontSize:"25px"}}>UserName</Typography>
   </Box>
 
